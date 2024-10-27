ed6b8a0588b5f4512b9015bd4aa4a95be616969d
ivan <ivanbeltran1610@gmail.com>
2024-10-26 18:49:14 -0600

    reestructurando archivos

---
+++ AdminitradorDeOrdenes/src/Main.kt
@@ -0,0 +1,48 @@
+import java.util.*
+
+import validations.ValidarString
+import validations.ValidarNumero
+import PupuseriaClass.Orden
+import PupuseriaClass.Pupusas
+import PupuseriaClass.Pupuseria
+
+
+fun mostrarOpciones() { //creando funcion para mostrar el menu de opciones
+    println("Bienvenido a la Pupusería \"El Comalito\"")
+    println("Seleccione una opción:")
+    println("1. Registrar una nueva orden")
+    println("2. Ver órdenes pendientes")
+    println("3. Despachar orden")
+    println("4. Ver órdenes despachadas")
+    println("5. Salir")
+    println("ingrese una opcion :")
+}
+
+fun main() {
+    val menuPupusas = listOf("revueltas", "frijol con queso", "queso")
+    val ordenesPendientes: Queue<Orden> = LinkedList()
+    val ordenesDespachadas: Stack<Orden> = Stack()
+    val pupuseria = Pupuseria(menuPupusas, ordenesPendientes, ordenesDespachadas) // clase con los metodos de una pupuseria
+
+    while (true) {
+        mostrarOpciones()
+        var opcionSeleccionada = readln()
+        println()
+        while (!ValidarNumero(opcionSeleccionada)){
+            println("Ingrese un numero valido: ")
+            opcionSeleccionada= readln()
+        }
+
+        when (opcionSeleccionada.toInt()) {
+            1 -> pupuseria.agregarOrden()
+            2 -> pupuseria.verOrdenesPendientes()
+            3 -> pupuseria.despacharOrden()
+            4 -> pupuseria.verOrdenesDespachadas()
+            5 -> {
+                println("Saliendo del programa.")
+                break
+            }
+            else -> println("Opción inválida, por favor intente de nuevo. \n")
+        }
+    }
+}
